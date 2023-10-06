95f3964358542d2691d459f1c6f8300d1ce2e0fb
Rostyslav Kravchenko <GlobalLogic_rostyslavk@nice.com>
2023-10-06 18:41:31 +0300

    10/06 18:41

--- basics/basics-05-using-the-native-event-object/basics-05-using-the-native-event-object/app.js
+++ basics/basics-05-using-the-native-event-object/basics-05-using-the-native-event-object/app.js
@@ -2,10 +2,54 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      //fullname: ''
     };
   },
+  watch: {
+    
+    //dataPropertyName(newValue, oldValue)
+    counter(value){
+      if(Math.abs(value) > 50){
+        const that = this;
+        setTimeout(function(){
+          that.counter = 0;
+        }, 2000);
+      }
+    }
+    // name(value){
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   }else{
+    //     this.fullname = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value){
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   }else{
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // }
+  },
+  computed: {
+    fullname(){
+      console.log("running again...")
+      if (this.name === '' && this.lastName === ''){
+        return '';
+      }
+      return this.name+' '+this.lastName; 
+    }
+  },
   methods: {
+    outputFullName(){
+      console.log("running again...")
+      if (this.name === ''){
+        return '';
+      }
+      return this.name+' '+"William";
+    },
     setName(event) {
       this.name = event.target.value;
     },
@@ -17,7 +61,9 @@ const app = Vue.createApp({
       // this.counter--;
     },
     resetName(){
+      console.log("reset")
       this.name = '';
+      this.lastName = '';
     }
   }
 });
