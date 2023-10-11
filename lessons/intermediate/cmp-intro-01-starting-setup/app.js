a830e0c6dec968dae22ba7fa6837e0cdaf2435cb
Rostyslav Kravchenko <GlobalLogic_rostyslavk@nice.com>
2023-10-11 21:00:59 +0300

    added lessons\intermediate\cmp-intro-01-starting-setup

---
+++ lessons/intermediate/cmp-intro-01-starting-setup/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            friends:[
+                {
+                    id: 'manuel',
+                    name: 'Manule Cold',
+                    phone: '3320348883274',
+                    email: 'man@lcoalhost.com'
+                },
+                {
+                    id: 'jul',
+                    name: 'Jul Jones',
+                    phone: '4720346663274',
+                    email: 'jul@lcoalhost.com'
+                }
+            ]
+        }
+    }
+})
