2ec6ce79a5623523e431549a1062271b6707fe68
rodiaua <rodiaua@gmail.com>
2023-10-12 23:20:56 +0300

    23:20 12 Oct

--- lessons/intermediate/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ lessons/intermediate/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,7 +1,8 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
-    <button @click="toggleDetails">{{detailsAreVisible? 'Hide' : 'Show'}} Details</button>
+    <h2>{{ friend.name }} {{friend.isFavorite?'(Favorite)':''}}</h2>
+    <button @click="toggleIsFavorite">{{ friend.isFavorite?'Remove from favorites':'Mark as favorite' }}</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
       <li><strong>Phone:</strong> {{ friend.phone }}</li>
       <li><strong>Email:</strong> {{ friend.email }}</li>
@@ -11,21 +12,28 @@
 
 <script>
 export default {
+  //props: ['friend'],
+  props: {
+    friend: Object
+  },
   data() {
     return {
       detailsAreVisible: false,
-      friend: {
-        id: "jack",
-        name: "Jack Willson",
-        phone: "380403 8304 30030",
-        email: "jack@mail.com",
-      },
     };
   },
   methods: {
-    toggleDetails(){
+    toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleIsFavorite(){
+      this.$emit('toggle-favorite', this.friend.id);
     }
   }
 };
 </script>
+
+<style>
+button{
+  margin: 1%;
+}
+</style>
