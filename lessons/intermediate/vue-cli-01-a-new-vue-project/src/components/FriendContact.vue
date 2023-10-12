d61ffbea856fe8e735097a6172960e657d8b1d9b
Rostyslav Kravchenko <GlobalLogic_rostyslavk@nice.com>
2023-10-12 21:28:12 +0300

    9:28 pm 12 oct

---
+++ lessons/intermediate/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{detailsAreVisible? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "jack",
+        name: "Jack Willson",
+        phone: "380403 8304 30030",
+        email: "jack@mail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails(){
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
