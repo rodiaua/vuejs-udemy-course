61d8e58e97ae3e8367db809e73445fa5e863e162
Rostyslav Kravchenko <GlobalLogic_rostyslavk@nice.com>
2023-12-20 20:23:58 +0200

    8:23 20 Dec 2023

--- projects/prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
+++ projects/prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -1,21 +1,21 @@
 <template>
-    <li>
-        <div>
-            <header>
-                <h3>{{ title }}</h3>
-                <button>Delete</button>
-            </header>
-        </div>
-        <p>{{ description }}</p>
-        <nav>
-            <a :href="link" target='_blank'>View Resource</a>
-        </nav>
-    </li>
+  <li>
+    <base-card>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link" target='_blank'>View Resource</a>
+      </nav>
+    </base-card>
+  </li>
 </template>
 
 <script>
 export default {
-    props: ['title', 'description', 'link']
+  props: ['title', 'description', 'link']
 }
 </script>
 
